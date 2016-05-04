a9ce5e6fcab74dcb74bca95edf0e846e23d673ac
Octavio Mata <Gabriel@Octavios-MacBook-Pro.local>
2016-05-04 12:04:01 -0400

    start jquery js

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+
+var questions = [{
+    question: "What Year did the french and indian war beging?",
+    choices: ["1964", "1974", "1984", "1699"],
+    correctAnswer: 1
+}, {
+    question: "Which President Was a U.S. Army Ranger?",
+    choices: ["George Bush", "George Washington", "Abraham Lincoln", "Andrew Jackson"],
+    correctAnswer: 2
+}, {
+    question: "In Which year did the Boston Tea Party take place?",
+    choices: ["1772", "1773", "1843", "1900"],
+    correctAnswer: 1
+}, {
+    question: "What is the longest river in the U.S?",
+    choices: ["Missouri", "Rio Grande", "Saint Johns", "Mississippi"],
+    correctAnswer: 0
+}, {
+    question: "What is the busiest subway station in NYC?",
+    choices: ["Times Square", "Grand Central", "Union Street", "231 st"],
+    correctAnswer: 0
+}];
+
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+
